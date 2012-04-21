5b6a4a6d1354fbd071bdfb819cf2f89ee34f4aa8
Andrew Tergis <theterg@gmail.com>
2012-04-20 22:21:08 -0400

    Simple peak value test.  Plan formulated:
    -continually find peak values
    -reset peak value on a short period (a few seconds)
    -average the peak value (at the end of the reset period)
    Boom, auto upper-bound calculator.

--- trafficlight_vu_serial/trafficlight_vu_serial.ino
+++ trafficlight_vu_serial/trafficlight_vu_serial.ino
@@ -14,8 +14,11 @@ int pin_red = 9;
 int pin_yellow = 10;
 int pin_green = 11;
 int pin_led = 13;
+int pin_audio = A0;
 
 int adc_buff[ADC_BUFFLEN];
+int adc_max = 0;
+int peak_counter = 0;
 
 void setup(){
   //Configure necessary I/O pins
@@ -30,12 +33,22 @@ void setup(){
   
   Timer1.initialize(2000);
   Timer1.attachInterrupt(timerIsr);
+  
+  Serial.begin(115200);
 }
 
 void loop(){
-  
+  if ((millis()%2000) == 0){
+    adc_max = 0;
+  }
+  Serial.println(adc_max);
 }
 
 void timerIsr(){
+  //For the moment, only using the first position of the adc buffer
+  adc_buff[0] = analogRead(pin_audio);
+  if (adc_buff[0] > adc_max){
+    adc_max = adc_buff[0];  
+  }
   digitalWrite(pin_led, digitalRead(pin_led) ^ 1);
 }
